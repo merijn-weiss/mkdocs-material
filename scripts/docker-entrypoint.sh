fcca5633e44bad0e55cc3658390a8b2ac909561c
Merijn Weiss <Merijn.Weiss@kyndryl.com>
2026-05-18 16:12:10 +0200

    Fix entry script

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -6,4 +6,19 @@ if [[ -f included_repos.txt ]]; then
     get-included-repos
 fi
 
+#
+# GitLab CI injects:
+#   sh -c "..."
+#
+# If first arg looks like a shell, execute directly.
+#
+
+if [[ "${1:-}" == "sh" ]] || [[ "${1:-}" == "/bin/sh" ]]; then
+    exec "$@"
+fi
+
+#
+# Default runtime behavior
+#
+
 exec mkdocs "$@"
